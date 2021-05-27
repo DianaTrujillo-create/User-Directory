e0d871f8038e7e958e546623580d6b9aa57aa4df
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-05-27 04:55:58 -0500

    Update DataArea.js

--- userdirectory/src/components/DataArea.js
+++ userdirectory/src/components/DataArea.js
@@ -1,4 +1,9 @@
 import react, {useState, useEffect} from "react";
 import API from "../utils/API";
 import DataAreaContext from "../utils/DataAreaContext";
-import Nav from "./Nav"
+import Nav from "../components/Nav";
+import DataTable from "../components/DataTable";
+
+const DataArea = () => {
+    
+}
