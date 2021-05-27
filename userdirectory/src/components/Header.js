ca6ada3ffca45ea55516edd837c4ec716af61749
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-05-27 00:16:34 -0500

    commit to main

---
+++ userdirectory/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "../styles/Header.css";
+
+const Header = () => {
+    return (
+        <div className = "container"> 
+        <h1>Employee Directory</h1>
+        <h4>Click on header filter or use the search box to narrow your results</h4>
+        </div>
+    );
+}
+
+export default Header;
